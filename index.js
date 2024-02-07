064fac512366b9dadc3799e73bf3f8e7e387efcb
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-02-07 21:36:04 +0700

    make some chenge in structure folder

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    req.timeRequest = Date.now();
+    console.log(req.method, req.url);
+    next();
+});
+
+const auth = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'somethingidk') {
+        next();
+    } else {
+        throw new Error('You are not authorized');
+    }
+};
+
+app.get('/admin', auth, (req, res) => {
+    res.send('Welcome to admin page');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/page', (req, res) => {
+    console.log('Time:', req.timeRequest);
+    res.send('Hello Page');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
